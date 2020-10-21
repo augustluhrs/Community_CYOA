ea05c2c1a51c9349a9c10fd7d1e9f8511cfafaaf
August Luhrs <augustluhrs@gmail.com>
2020-10-21 17:55:41 -0400

    database test!

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 Discord bot for a community-generated Choose-Your-Own-Adventure story where whenever a reader gets to a dead-end node, they have to write that section of the story.
 
+Made with lots of help from Dan Shiffman's Discord Bot Tutorials!
+Based on [this one](https://github.com/CodingTrain/Discord-Bot-Choo-Choo) in particular.
 
 ### TODO
 
 First Steps
 - ~~hello world~~
 - basic story response structure
-- private message or in channel?
+- ~~private message~~
 - save story to file and read from file
 
 
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//database stuff
+const Datastore = require('nedb');
+// var db = new Datastore({filename: "dataTest.db", autoload: true});
+var db = new Datastore({filename: "secret_stories/testStory.db", autoload: true});
+
+db.update({type: "globalData"}, {$set: {"timeLastUsed": Date.now()}}, {upsert: true}, function(err, upsert){
+    console.log("err: " + err);
+    console.log("upsert: " + upsert);
+});
+//not sure why above is creating two diff docs with same id...
+
+// Discord stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -16,12 +28,45 @@ client.on('message', gotMessage);
 function gotMessage(msg) {
   console.log(msg.content);
   // Only for this server and this channel
-  if (msg.guild.id === serverID && msg.channel.id === channelID) {
+//   if (msg.guild.id === serverID && msg.channel.id === channelID) {
+  if (msg.channel.id === channelID) {
     if (msg.content === 'ping') {
       msg.channel.send('pong');
     }
     if(msg.content === '!start'){
         msg.channel.send('starting!');
+        // console.log(msg.author.id);
+        // client.users.cache.get(msg.author.id).send('hey there bb');
+
+        // db.insert({name: msg.author.username, id: msg.author.id },
+        //     function(err, newDocs){
+        //         console.log('err: ' + err);
+        //         console.log('newDocs: ' + newDocs);
+        // });
+
+        // db.find({},function(err, docs) { //why called docs? what's better name?
+        //     for(var i = 0; i < docs.length; i ++){
+        //         //need to make sure everything added has a type
+        //         if(docs[i].type == "globalData"){
+        //             console.log("count: " + docs[i].nodeNum);
+                    
+        //         }
+        //     }
+    // })   
+        
+        db.find({type: "globalData"}, function(err, docs) { //why called docs? what's better name?
+            console.log(docs[0].nodeNum);
+            db.update(
+                {_id: docs[0]._id}, 
+                // {$set: {"nodeNum": docs.nodeNum++}} 
+                {$inc: {"nodeNum": 1}},
+                {upsert: true}
+                //no callback for now?
+            );
+        });
+        // client.users.cache.get(msg.author.id).send('hey there bb');
+
+
     }
   }
 }
